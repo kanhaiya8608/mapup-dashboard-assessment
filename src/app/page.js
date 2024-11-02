ab89c7fc2745eb2f2214d2a245ad8ecb938bb369
Kanhaiya Verma <kanhaiya8608@gmail.com>
2024-11-02 16:09:40 +0530

    fixed page

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,27 @@
 import Dashboard from "@/components/dashboard";
 
 const fetchData = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/evData`, {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/data-to-visualize/Electric_Vehicle_Population_Data.csv`, {
     cache: 'no-store',
   });
-  const evData = await res.json();
+  const csvText = await res.text();
+
+  const rows = csvText.split('\n');
+  const headers = rows[0].split(',');
+  const evData = rows.slice(1).map(row => {
+    const values = row.split(',');
+    const obj = {};
+    headers.forEach((header, index) => {
+      obj[header] = values[index];
+    });
+    return obj;
+  });
+
   return evData;
 };
 
 export default async function DashboardPage() {
-  const evData = await fetchData(); 
+  const evData = await fetchData();
 
   return (
     <Dashboard evData={evData} />
